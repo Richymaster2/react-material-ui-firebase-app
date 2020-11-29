181f3b3e86fba4cf8b56c021865b4ab738b55f6d
Richymaster2 <ngetichrichard9@gmail.com>
2020-11-29 16:41:21 +0300

    setup material ui with create-react app with react-router routes

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Typography, Paper, Avatar, Button} from '@material-ui/core'
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import VerifiedUserOutlined from '@material-ui/icons/VerifiedUserOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        display: 'block', 
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.primary.main,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+})
+
+function HomePage(props){
+    const { classes } = props
+
+    return(
+       <main className={classes.main}>
+            <Paper className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <VerifiedUserOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Hi Lucky Farm User
+                </Typography>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="outlined"
+                    color="primary"
+                    className={classes.submit}>
+                    Register
+                </Button>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}>
+                    Login
+                </Button>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    className={classes.submit}>
+                    Dashboard
+                </Button>
+            </Paper>
+        </main>
+    )
+}
+
+export default  withStyles(styles)(HomePage)
